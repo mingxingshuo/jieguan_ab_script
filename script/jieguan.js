7b404746f7b8b12eff5f884ccdb96a84b8bc495d
Administrator <lionel@buzzy.cc>
2019-04-30 17:11:23 +0800

    随机时间调起

--- script/jieguan.js
+++ script/jieguan.js
@@ -9,7 +9,8 @@ const mem = require("../util/mem")
 var code = parseInt(process.env.code)
 
 var rule = new schedule.RecurrenceRule();
-var times = [1, 11, 21, 31, 41, 51];
+let start = parseInt(Math.random()*10)
+var times = [start, start+10, start+20, start+30, start+40, start+50];
 rule.second = times;
 schedule.scheduleJob(rule, async function () {
     let follow_flag = await mem.get('big_follow_flag_' + code)
@@ -20,7 +21,8 @@ schedule.scheduleJob(rule, async function () {
 })
 
 var rule1 = new schedule.RecurrenceRule();
-var times1 = [1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51, 56];
+start = parseInt(Math.random()*5)
+var times1 = [start, start+5*1, start+5*2, start+5*3, start+5*4, start+5*5, start+5*6, start+5*7, start+5*8, start+5*9, start+5*10, start+5*11];
 rule1.second = times1;
 schedule.scheduleJob(rule1, async function () {
     let user_flag = await mem.get('big_user_flag_' + code)
@@ -32,7 +34,8 @@ schedule.scheduleJob(rule1, async function () {
 })
 
 var rule2 = new schedule.RecurrenceRule();
- var times2 = [1, 6, 11, 16, 21, 26, 31, 36, 41, 46, 51, 56];
+start = parseInt(Math.random()*5)
+var times2 = [start, start+5*1, start+5*2, start+5*3, start+5*4, start+5*5, start+5*6, start+5*7, start+5*8, start+5*9, start+5*10, start+5*11];
  rule2.second = times2;
  schedule.scheduleJob(rule2, async function () {
      let tag_flag = await mem.get('big_tag_female_flag_' + code)
