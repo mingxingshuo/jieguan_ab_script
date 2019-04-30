18baaacc5aa108c0c414dabdede04306e66b82cd
Administrator <lionel@buzzy.cc>
2019-04-30 14:01:29 +0800

    1

--- util/clear.js
+++ util/clear.js
@@ -13,23 +13,17 @@ async function clear(code){
     let conf = await ConfigModel.findOne({code: code})
     let appid = conf.appid
     let client = await wechat_util.getClient(code)
-    let clear_data = await client_clear(client,appid)
-    console.log('-------清空调用次数返回------')
-    console.log(clear_data)
+    client.clearQuota(appid, function (err, data) {
+            console.log('-------清空调用次数返回------')
+            console.log(data)
+    });
+    
 	setTimeout(function () {
+        console.log('-------重置清空flag------')
         mem.set('dahao_script_clear_'+code,'',60).then(function () {
 
         })
     },30*1000)
 };
-
-function client_clear(client,appid){
-    return new Promise((resolve, reject)=>{
-      	client.clearQuota(appid, function (err, data) {
-	        resolve(data)
-	   	});
-    });
-}
-
 module.exports.clear = clear
 
