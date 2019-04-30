7807439937e7829c1f4855c68141787cb287fffa
Administrator <lionel@buzzy.cc>
2019-04-30 10:52:50 +0800

    1

--- script/clear_mem.js
+++ script/clear_mem.js
@@ -1,8 +1,7 @@
 const mem = require("../util/mem")
 const UserconfModel = require('../model/Userconf');
-const code =84
 
-async function a() {
+async function a(code) {
     let count =await UserconfModel.count({code:code,sex:'0'})
     if(count>=50){
     	await mem.set("big_tag_unknow_flag_" + code, 0, 60)
--- script/jieguan.js
+++ script/jieguan.js
@@ -6,7 +6,7 @@ const male = require('./test_tag_male')
 const unknow = require('./test_tag_unknow')
 const clear_mem = require('./clear_mem')
 const mem = require("../util/mem")
-var code = 84
+var code = parseInt(process.env.code)
 
 var rule = new schedule.RecurrenceRule();
 var times = [1, 11, 21, 31, 41, 51];
@@ -59,8 +59,8 @@ schedule.scheduleJob(rule2, async function () {
 })
 
 var rule3 = new schedule.RecurrenceRule();
-rule3.second = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59]
+rule3.second = [0]
 
 schedule.scheduleJob(rule3, async function () {
-     await clear_mem.a()
+     await clear_mem.a(code)
  })
