1c21dd7b580b3369618c0c7ac58deb1e2a19cba7
efdssdf <efdssdf@gmail.com>
2019-05-01 17:24:56 +0800

    'fix'

--- script/test_user.js
+++ script/test_user.js
@@ -26,76 +26,79 @@ async function update_user(_id, code) {
             await mem.set("big_user_flag_" + code, 0, 60 * 60)
             return
         } else {
-            b_user(user_arr,code,users,client);
-            (function(users,code){
-                setTimeout(function(){
+            b_user(user_arr, code, users, client);
+            (function (users, code) {
+                setTimeout(function () {
                     if (users.length == 100) {
                         update_user(users[99]._id, code);
+                        mem.set('big_user_ending_' + code, 0, 1).then(function () {
+
+                        })
                         console.log(code + '-------user-countinue')
                     } else {
-                         mem.set('big_user_ending_' + code, 1, 7 * 24 * 60 * 60).then(function(){
+                        mem.set('big_user_ending_' + code, 1, 7 * 24 * 60 * 60).then(function () {
 
-                         })
+                        })
                         console.log(code + '-------user---end')
                         //return
                     }
-                },500)   
-            })(users,code)
+                }, 500)
+            })(users, code)
         }
     })
 }
 
-async function b_user(user_arr,code,users,client){
+async function b_user(user_arr, code, users, client) {
     client.batchGetUsers(user_arr, async function (err, data) {
-                if (err) {
-                    //update_user(users[99]._id, code);
-                } else {
-                    if (data.errcode) {
-                        await RecordModel.findOneAndUpdate({code: code}, {
+        if (err) {
+            //update_user(users[99]._id, code);
+        } else {
+            if (data.errcode) {
+                await RecordModel.findOneAndUpdate({code: code}, {
+                    code: code,
+                    user_openid: user_arr[0],
+                    user_errcode: data.errcode
+                }, {upsert: true})
+                return
+            }
+            if (data && data.user_info_list) {
+                let userArr = []
+                async.eachLimit(data.user_info_list, 100, function (info, callback) {
+                    if (info.nickname) {
+                        userArr.push({
                             code: code,
-                            user_openid: user_arr[0],
-                            user_errcode: data.errcode
-                        }, {upsert: true})
-                        return
-                    }
-                    if (data && data.user_info_list) {
-                        let userArr = []
-                        async.eachLimit(data.user_info_list, 100, function (info, callback) {
-                            if (info.nickname) {
-                                userArr.push({
-                                    code: code,
-                                    openid: info.openid,
-                                    sex: info.sex.toString()
-                                })
-                            }
-                            callback(null)
-                        }, function (error) {
-                            if (error) {
-                                console.log(error, '--------------error')
-                                //return update_user(_id, code);
-                            }
-                            UserconfModel.insertMany(userArr, async function (error, docs) {
-                                if (error) {
-                                    console.log('------insertMany error--------');
-                                    console.log(error);
-                                    console.log('------------------------------');
-                                    //return update_user(_id, code);
-                                }
-                                
-                                OpenidModel.remove({code: code, openid: {$in: user_arr}}, function () {
-
-                                })
-                                await RecordModel.findOneAndUpdate({code: code}, {
-                                    user_openid: user_arr[user_arr.length - 1],
-                                    $inc: {user_count: user_arr.length}
-                                }, {upsert: true})
-                            })
+                            openid: info.openid,
+                            sex: info.sex.toString()
                         })
-                    } else {
-                       // update_user(users[99]._id, code);
                     }
-                }
-            })
+                    callback(null)
+                }, function (error) {
+                    if (error) {
+                        console.log(error, '--------------error')
+                        //return update_user(_id, code);
+                    }
+                    UserconfModel.insertMany(userArr, async function (error, docs) {
+                        if (error) {
+                            console.log('------insertMany error--------');
+                            console.log(error);
+                            console.log('------------------------------');
+                            //return update_user(_id, code);
+                        }
+
+                        OpenidModel.remove({code: code, openid: {$in: user_arr}}, function () {
+
+                        })
+                        await RecordModel.findOneAndUpdate({code: code}, {
+                            user_openid: user_arr[user_arr.length - 1],
+                            $inc: {user_count: user_arr.length}
+                        }, {upsert: true})
+                    })
+                })
+            } else {
+                // update_user(users[99]._id, code);
+            }
+        }
+    })
 }
 
 module.exports = {get_user: get_user}
