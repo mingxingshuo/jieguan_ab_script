d957a42c83a9959db1ae064b93384d850a03c107
efdssdf <efdssdf@gmail.com>
2019-05-01 11:26:31 +0800

    'fix'

--- script/clear_mem.js
+++ script/clear_mem.js
@@ -2,9 +2,10 @@ const mem = require("../util/mem")
 const UserconfModel = require('../model/Userconf');
 
 async function a(code) {
-    let count =await UserconfModel.count({code:code,sex:'0'})
-    if(count>=50){
-    	await mem.set("big_tag_unknow_flag_" + code, 0, 60)
+    console.log('-------clear unknow tag---------')
+    let count = await UserconfModel.count({code: code, sex: '0'})
+    if (count >= 50) {
+        await mem.set("big_tag_unknow_flag_" + code, 0, 1)
     }
 }
 
--- script/jieguan.js
+++ script/jieguan.js
@@ -9,8 +9,8 @@ const mem = require("../util/mem")
 var code = parseInt(process.env.code)
 
 var rule = new schedule.RecurrenceRule();
-let start = parseInt(Math.random()*10)
-var times = [start, start+10, start+20, start+30, start+40, start+50];
+let start = parseInt(Math.random() * 10)
+var times = [start, start + 10, start + 20, start + 30, start + 40, start + 50];
 rule.second = times;
 schedule.scheduleJob(rule, async function () {
     let follow_flag = await mem.get('big_follow_flag_' + code)
@@ -21,12 +21,12 @@ schedule.scheduleJob(rule, async function () {
 })
 
 var rule1 = new schedule.RecurrenceRule();
-start = parseInt(Math.random()*5)
-var times1 = [start, start+5*1, start+5*2, start+5*3, start+5*4, start+5*5, start+5*6, start+5*7, start+5*8, start+5*9, start+5*10, start+5*11];
+start = parseInt(Math.random() * 5)
+var times1 = [start, start + 5 * 1, start + 5 * 2, start + 5 * 3, start + 5 * 4, start + 5 * 5, start + 5 * 6, start + 5 * 7, start + 5 * 8, start + 5 * 9, start + 5 * 10, start + 5 * 11];
 rule1.second = times1;
 schedule.scheduleJob(rule1, async function () {
     let user_flag = await mem.get('big_user_flag_' + code)
-    
+
     if (!user_flag) {
         user.get_user(code)
         await mem.set("big_user_flag_" + code, 1, 60 * 60)
@@ -34,27 +34,27 @@ schedule.scheduleJob(rule1, async function () {
 })
 
 var rule2 = new schedule.RecurrenceRule();
-start = parseInt(Math.random()*5)
-var times2 = [start, start+5*1, start+5*2, start+5*3, start+5*4, start+5*5, start+5*6, start+5*7, start+5*8, start+5*9, start+5*10, start+5*11];
- rule2.second = times2;
- schedule.scheduleJob(rule2, async function () {
-     let tag_flag = await mem.get('big_tag_female_flag_' + code)
-     if (!tag_flag) {
-         female.tag(code)
-         await mem.set("big_tag_female_flag_" + code, 1, 24 * 60 * 60)
-     }
- })
+start = parseInt(Math.random() * 5)
+var times2 = [start, start + 5 * 1, start + 5 * 2, start + 5 * 3, start + 5 * 4, start + 5 * 5, start + 5 * 6, start + 5 * 7, start + 5 * 8, start + 5 * 9, start + 5 * 10, start + 5 * 11];
+rule2.second = times2;
+schedule.scheduleJob(rule2, async function () {
+    let tag_flag = await mem.get('big_tag_female_flag_' + code)
+    if (!tag_flag) {
+        female.tag(code)
+        await mem.set("big_tag_female_flag_" + code, 1, 60 * 60)
+    }
+})
 schedule.scheduleJob(rule2, async function () {
     let tag_flag = await mem.get('big_tag_male_flag_' + code)
     if (!tag_flag) {
         male.tag(code)
-        await mem.set("big_tag_male_flag_" + code, 1, 24 * 60 * 60)
+        await mem.set("big_tag_male_flag_" + code, 1, 60 * 60)
     }
 })
 schedule.scheduleJob(rule2, async function () {
     let tag_flag = await mem.get('big_tag_unknow_flag_' + code)
-    console.log('-------------未知_flag----',tag_flag)
-    console.log('-------未知 flag-------',!tag_flag)
+    console.log('-------------未知_flag----', tag_flag)
+    console.log('-------未知 flag-------', !tag_flag)
     if (!tag_flag) {
         unknow.tag(code)
         await mem.set("big_tag_unknow_flag_" + code, 1, 60 * 60)
@@ -62,8 +62,8 @@ schedule.scheduleJob(rule2, async function () {
 })
 
 var rule3 = new schedule.RecurrenceRule();
-rule3.second = [0]
+rule3.second = [0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55]
 
 schedule.scheduleJob(rule3, async function () {
-     await clear_mem.a(code)
- })
+    await clear_mem.a(code)
+})
