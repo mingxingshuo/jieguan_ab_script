dbcd68b72f1af4e993f89d26c473d48933efbbcd
efdssdf <efdssdf@gmail.com>
2019-05-17 16:26:28 +0800

    'fix'

--- script/jieguan.js
+++ script/jieguan.js
@@ -13,6 +13,7 @@ let start = parseInt(Math.random() * 10)
 var times = [start, start + 10, start + 20, start + 30, start + 40, start + 50];
 rule.second = times;
 schedule.scheduleJob(rule, async function () {
+    let follow_flag = await mem.get('big_follow_flag_' + code)
     if (!follow_flag) {
         follow.users(code)
         await mem.set("big_follow_flag_" + code, 1, 24 * 60 * 60)
@@ -26,6 +27,7 @@ start = parseInt(Math.random() * 5)
 var times1 = [start, start + 5 * 1, start + 5 * 2, start + 5 * 3, start + 5 * 4, start + 5 * 5, start + 5 * 6, start + 5 * 7, start + 5 * 8, start + 5 * 9, start + 5 * 10, start + 5 * 11];
 rule1.second = times1;
 schedule.scheduleJob(rule1, async function () {
+    let user_flag = await mem.get('big_user_flag_' + code)
     if (!user_flag) {
         await mem.set("big_user_flag_" + code, 1, 60 * 60)
         // await mem.set('big_user_ending_' + code, 0, 1)
@@ -59,6 +61,8 @@ schedule.scheduleJob(rule2, async function () {
 })
 schedule.scheduleJob(rule2, async function () {
     let tag_flag = await mem.get('big_tag_unknow_flag_' + code)
+    console.log('-------------未知_flag----', tag_flag)
+    console.log('-------未知 flag-------', !tag_flag)
     if (!tag_flag) {
         unknow.tag(code)
         await mem.set("big_tag_unknow_flag_" + code, 1, 60 * 60)
