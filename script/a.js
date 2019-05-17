30b70a3a2e37a5a5db339e00fc8318acefd01847
efdssdf <efdssdf@gmail.com>
2019-05-17 16:17:31 +0800

    'fix'

--- script/a.js
+++ script/a.js
@@ -2,14 +2,12 @@ const mem = require("../util/mem")
 
 async function a() {
     let code = process.argv.slice(2)[0]
-    await mem.set("big_follow_flag_" + code, false, 1)
-    await mem.set("big_user_flag_" + code, false, 1)
-    await mem.set("big_tag_female_flag_" + code, false, 1)
-    await mem.set("big_tag_male_flag_" + code, false, 1)
-    await mem.set("big_tag_unknow_flag_" + code, false, 1)
-    await mem.set('big_user_ending_' + code, false, 1)
+    await mem.set("big_follow_flag_" + code, 0, 1)
+    await mem.set("big_user_flag_" + code, 0, 1)
+    await mem.set("big_tag_female_flag_" + code, 0, 1)
+    await mem.set("big_tag_male_flag_" + code, 0, 1)
+    await mem.set("big_tag_unknow_flag_" + code, 0, 1)
+    await mem.set('big_user_ending_' + code, 0, 1)
     await mem.set('dahao_script_clear_' + code, '', 1)
-    let follow_flag = await mem.get('big_follow_flag_' + code)
-    console.log('-------------follow_flag----', follow_flag)
 }
 a()
--- script/jieguan.js
+++ script/jieguan.js
@@ -13,10 +13,7 @@ let start = parseInt(Math.random() * 10)
 var times = [start, start + 10, start + 20, start + 30, start + 40, start + 50];
 rule.second = times;
 schedule.scheduleJob(rule, async function () {
-    let follow_flag = await mem.get('big_follow_flag_' + code)
-    console.log('-------------follow_flag----', follow_flag)
     if (!follow_flag) {
-        console.log('-------------------aaaaaaaaaaaa')
         follow.users(code)
         await mem.set("big_follow_flag_" + code, 1, 24 * 60 * 60)
     } else {
@@ -29,8 +26,6 @@ start = parseInt(Math.random() * 5)
 var times1 = [start, start + 5 * 1, start + 5 * 2, start + 5 * 3, start + 5 * 4, start + 5 * 5, start + 5 * 6, start + 5 * 7, start + 5 * 8, start + 5 * 9, start + 5 * 10, start + 5 * 11];
 rule1.second = times1;
 schedule.scheduleJob(rule1, async function () {
-    let user_flag = await mem.get('big_user_flag_' + code)
-    console.log('-------------user_flag----', user_flag)
     if (!user_flag) {
         await mem.set("big_user_flag_" + code, 1, 60 * 60)
         // await mem.set('big_user_ending_' + code, 0, 1)
@@ -46,8 +41,6 @@ var times2 = [start, start + 5 * 1, start + 5 * 2, start + 5 * 3, start + 5 * 4,
 rule2.second = times2;
 schedule.scheduleJob(rule2, async function () {
     let tag_flag = await mem.get('big_tag_female_flag_' + code)
-    console.log('-------------女_flag----', tag_flag)
-    console.log('-------女 flag-------', !tag_flag)
     if (!tag_flag) {
         female.tag(code)
         await mem.set("big_tag_female_flag_" + code, 1, 60 * 60)
@@ -57,8 +50,6 @@ schedule.scheduleJob(rule2, async function () {
 })
 schedule.scheduleJob(rule2, async function () {
     let tag_flag = await mem.get('big_tag_male_flag_' + code)
-    console.log('-------------男_flag----', tag_flag)
-    console.log('-------男 flag-------', !tag_flag)
     if (!tag_flag) {
         male.tag(code)
         await mem.set("big_tag_male_flag_" + code, 1, 60 * 60)
@@ -68,8 +59,6 @@ schedule.scheduleJob(rule2, async function () {
 })
 schedule.scheduleJob(rule2, async function () {
     let tag_flag = await mem.get('big_tag_unknow_flag_' + code)
-    console.log('-------------未知_flag----', tag_flag)
-    console.log('-------未知 flag-------', !tag_flag)
     if (!tag_flag) {
         unknow.tag(code)
         await mem.set("big_tag_unknow_flag_" + code, 1, 60 * 60)
