6bc0ed6a9daa67d884745ae07b834293d4fc9396
efdssdf <efdssdf@gmail.com>
2019-05-20 20:05:24 +0800

    'fix'

---
+++ script/c.js
@@ -0,0 +1,31 @@
+const mem = require("./mem")
+var wechat_util = require('../util/get_weichat_client.js')
+var ConfigModel = require('../model/Config');
+
+async function clear(){
+    let code = process.argv.slice(2)[0]
+    console.log('-------清空调用次数flag------')
+    let flag = await mem.get('dahao_script_clear_'+code)
+    console.log(flag)
+    if(flag){
+        return
+    }
+    await mem.set('dahao_script_clear_'+code,'1',60)
+    let conf = await ConfigModel.findOne({code: code})
+    let appid = conf.appid
+    let client = await wechat_util.getClient(code)
+    client.clearQuota(appid, function (err, data) {
+        console.log('-------清空调用次数返回------')
+        console.log(data)
+    });
+
+    setTimeout(function () {
+        console.log('-------重置清空flag------')
+        mem.set('dahao_script_clear_'+code,'',60).then(function () {
+
+        })
+    },30*1000)
+};
+
+clear()
+
