8d628d6369d313c476dd9d2f2f857835c38bd797
efdssdf <efdssdf@gmail.com>
2019-05-21 16:27:23 +0800

    'clear'

--- util/clear.js
+++ util/clear.js
@@ -2,28 +2,33 @@ const mem = require("./mem")
 var wechat_util = require('../util/get_weichat_client.js')
 var ConfigModel = require('../model/Config');
 
-async function clear(code){
+async function clear(code) {
     console.log('-------清空调用次数flag------')
-	let flag = await mem.get('dahao_script_clear_'+code)
+    let flag = await mem.get('dahao_script_clear_' + code)
+    let times = await mem.get('dahao_script_clear_times' + code) || 0
     console.log(flag)
-	if(flag){
-		return
-	}
-	await mem.set('dahao_script_clear_'+code,'1',60)
+    if (flag) {
+        return
+    }
+    if(times>=2){
+        return
+    }
+    await mem.set('dahao_script_clear_' + code, '1', 60)
+    await mem.set('dahao_script_clear_times_' + code, times + 1, 24 * 60 * 60)
     let conf = await ConfigModel.findOne({code: code})
     let appid = conf.appid
     let client = await wechat_util.getClient(code)
     client.clearQuota(appid, function (err, data) {
-            console.log('-------清空调用次数返回------')
-            console.log(data)
+        console.log('-------清空调用次数返回------')
+        console.log(data)
     });
-    
-	setTimeout(function () {
+
+    setTimeout(function () {
         console.log('-------重置清空flag------')
-        mem.set('dahao_script_clear_'+code,'',60).then(function () {
+        mem.set('dahao_script_clear_' + code, '', 60).then(function () {
 
         })
-    },30*1000)
+    }, 30 * 1000)
 };
 module.exports.clear = clear
 
