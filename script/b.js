ce3ad0b465384157b1247e436d7b007a45ffe4e1
efdssdf <efdssdf@gmail.com>
2019-07-17 10:30:30 +0800

    'script'

--- script/b.js
+++ script/b.js
@@ -1,7 +1,10 @@
 var wechat_util = require('../util/get_weichat_client.js')
+var ConfigModel = require('../model/Config');
 
 async function a() {
     let code = process.argv.slice(2)[0]
+    let conf = await ConfigModel.findOne({code: code})
+    let appid = conf.appid
     let client = await wechat_util.getClient(code)
     client.clearQuota(appid, async function (err, data) {
         console.log(data,'-------------------data')
