4ab4abf8c4905139b12e5ca67b830c2cd2c56318
efdssdf <efdssdf@gmail.com>
2019-08-16 11:26:56 +0800

    'test'

--- util/clear.js
+++ util/clear.js
@@ -21,8 +21,7 @@ async function clear(code) {
     let client = await wechat_util.getClient(code)
     client.clearQuota(appid, async function (err, data) {
         console.log('-------清空调用次数返回------')
-        console.log(data)
-        if (data && data.errcode == 48006) {
+        if (err.code == 48006) {
             await mem.set('dahao_script_clear_limit_' + code, 1, 24 * 60 * 60)
         } else {
             await mem.set('dahao_script_clear_' + code, '1', 60)
