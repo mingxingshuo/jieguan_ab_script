4cdfc9f3b8b7105c99bb13490ef546b9ef403300
efdssdf <efdssdf@gmail.com>
2019-08-27 19:19:52 +0800

    'fix'

--- util/get_weichat_client.js
+++ util/get_weichat_client.js
@@ -1,8 +1,9 @@
 const ConfigModel = require('../model/Config');
 const mem = require('../util/mem.js');
-const Singleton = require('/home/work/tuitui_cms/tuitui_weichat/util/get_weichat_client');
+const Singleton = require('/home/work/tuitui_cms/tuitui_weichat/util/get_weichat_client.js');
 
 async function getClient(code) {
+    console.log(Singleton,'---------------------Single')
     let appid = await mem.get("configure_" + code)
     if(!appid){
         let conf = await ConfigModel.findOne({code:code})
