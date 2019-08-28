2cc886b5eed0e7c3da7c609920c96f059326a9db
efdssdf <efdssdf@gmail.com>
2019-08-28 15:47:12 +0800

    'fix'

--- util/get_weichat_client.js
+++ util/get_weichat_client.js
@@ -1,6 +1,6 @@
 const ConfigModel = require('../model/Config');
 const mem = require('../util/mem.js');
-const Singleton = require('/home/work/tuitui_cms/tuitui_weichat/util/get_weichat_client.js');
+const Singleton = require('/home/work/refresh/util/get_weichat_client.js');
 
 async function getClient(code) {
     let appid = await mem.get("configure_" + code)
@@ -10,6 +10,7 @@ async function getClient(code) {
         await mem.set("configure_" + code, appid, 30 * 24 * 3600)
     }
     let token = mem.get('access_token_'+appid)
+    console.log(token,'--------------------------token')
     let api = Singleton.getInterface(appid)
     api.store = {accessToken: token, expireTime: Date.now() + (expires_in - 10) * 1000}
     api.token = {accessToken: token, expireTime: Date.now() + (expires_in - 10) * 1000}
