e1c66f23a9c17f5be978758a660849d39a035403
efdssdf <efdssdf@gmail.com>
2019-08-28 18:15:58 +0800

    'fix'

--- util/mem.js
+++ util/mem.js
@@ -1,39 +1,10 @@
 var Memcached = require('memcached');
 var memcached = new Memcached('127.0.0.1:11211');
-
-module.exports.getContent = function (ticket) {
-    return new Promise((resolve, reject) => {
-        console.log(ticket, '-----------ticket1')
-        memcached.get('user_' + ticket, function (err, content) {
-            console.log(content, '-----------------content1')
-            resolve(content)
-        });
-    })
-}
-
-module.exports.timeContent = function (openid) {
-    return new Promise((resolve, reject) => {
-        console.log(openid, '-----------usertime')
-        memcached.get('usertime_' + openid, function (err, content) {
-            console.log(content, '-----------------timecontent')
-            resolve(content)
-        });
-    })
-}
-
-module.exports.bindContent =  function (openid) {
-    return new Promise((resolve, reject) => {
-        console.log(openid, '-----------bindtime')
-        memcached.get('bindtime_' + openid, function (err, content) {
-            console.log(content, '-----------------bindcontent')
-            resolve(content)
-        });
-    })
-}
+let prefix = require('../conf/proj.json').memcache_prefix;
 
 module.exports.get = function(key){
     return new Promise((resolve, reject)=>{
-        memcached.get(key,function(err,value){
+        memcached.get(prefix+"_"+key,function(err,value){
             resolve(value);
         });
     });
@@ -41,7 +12,7 @@ module.exports.get = function(key){
 
 module.exports.set = function(key,value,time){
     return new Promise((resolve, reject)=>{
-        memcached.set(key,value,time,function(err,value){
+        memcached.set(prefix+"_"+key,value,time,function(err,value){
             resolve(value);
         });
     });
