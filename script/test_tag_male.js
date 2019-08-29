c039eb4affbc07e036b424e5e362b3b4450f8ff8
efdssdf <efdssdf@gmail.com>
2019-08-29 16:21:31 +0800

    'fix'

--- script/test_tag_male.js
+++ script/test_tag_male.js
@@ -15,15 +15,26 @@ function get_tag(_id, code, tagId, sex) {
         update_tag(_id, code, tagId, sex, get_tag);
     } else {
         console.log('------男----------update_tag end');
-        mem.set("big_tag_male_flag_" + code, 0, 1)
+        mem.set("big_tag_male_flag_" + code, 0, 1).then(function () {
+
+        })
         return
     }
 }
 
 function update_tag(_id, code, tagId, sex, next) {
     UserconfModel.fetchTag(_id, code, sex, async function (error, users) {
+        if (users.length < 50) {
+            // let end = await mem.get('big_user_ending_' + code)
+            // if (!end) {
+                await mem.set("big_tag_male_flag_" + code, 0, 1)
+                return next(null, null, null, null)
+            // } else {
+            //     return next(null, null, null, null)
+            // }
+        }
         console.log('-------男  打标签---------')
-        let user_arr = [];
+        var user_arr = [];
         users.forEach(function (user) {
             user_arr.push(user.openid)
         })
@@ -31,37 +42,6 @@ function update_tag(_id, code, tagId, sex, next) {
         if (user_arr.length == 0) {
             console.log(user_arr, '-------------------user null')
             return next(null, null, null, null)
-        } else if (user_arr.length < 50) {
-            client.membersBatchtagging(tagId, user_arr, async function (error, res) {
-                if (error) {
-                    console.log('----男 打标签 error------')
-                    if (error.code == 45009) {
-                        Mclear.clear(code);
-                        return
-                    } else {
-                        if (error.code == 45159) {
-                            console.log('tagId----------', tagId)
-                        }
-                        return next(null, null, null, null)
-                    }
-                }
-                if (res.errcode) {
-                    console.log('----男 打标签 res error------')
-                    console.log(res)
-                    await RecordModel.findOneAndUpdate({code: code}, {
-                        code: code,
-                        tag_openid: user_arr[0],
-                        tag_errcode: res.errcode
-                    }, {upsert: true})
-                    return next(null, null, null, null)
-                }
-                await UserconfModel.remove({code: code, openid: {$in: user_arr}})
-                await RecordModel.findOneAndUpdate({code: code}, {
-                    tag_openid: user_arr[user_arr.length - 1],
-                    $inc: {tag_count: user_arr.length}
-                }, {upsert: true})
-                return next(null, null, null, null)
-            })
         } else {
             client.membersBatchtagging(tagId, user_arr, async function (error, res) {
                 if (error) {
@@ -71,7 +51,7 @@ function update_tag(_id, code, tagId, sex, next) {
                         return
                     } else {
                         if (error.code == 45159) {
-                            console.log('tagId----------', tagId)
+                            console.log('tagId----------',tagId)
                         }
                         return next(users[49]._id, code, tagId, sex);
                     }
