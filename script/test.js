555067b1c657e0d2abf0a7deefbf7d3203f438f0
efdssdf <efdssdf@gmail.com>
2019-11-19 16:04:27 +0800

    'script'

--- script/test.js
+++ script/test.js
@@ -2,7 +2,7 @@ var wechat_util = require('../util/get_weichat_client.js')
 var ConfigModel = require('../model/Config');
 
 async function a() {
-    // let code = process.argv.slice(2)[0]
+    let code = process.argv.slice(2)[0]
     // let conf = await ConfigModel.findOne({code: code})
     // let appid = conf.appid
     // let client = await wechat_util.getClient(code)
