a6e7ca2885a49c158fa94ffd2773076351bf3a5b
efdssdf <efdssdf@gmail.com>
2019-11-19 16:03:29 +0800

    'script'

--- script/test.js
+++ script/test.js
@@ -2,13 +2,15 @@ var wechat_util = require('../util/get_weichat_client.js')
 var ConfigModel = require('../model/Config');
 
 async function a() {
-    let code = process.argv.slice(2)[0]
-    let conf = await ConfigModel.findOne({code: code})
-    let appid = conf.appid
-    let client = await wechat_util.getClient(code)
-    client.clearQuota(appid, async function (err, data) {
-        console.log(err, '----------err')
-        console.log(data, '----------data')
-    })
+    // let code = process.argv.slice(2)[0]
+    // let conf = await ConfigModel.findOne({code: code})
+    // let appid = conf.appid
+    // let client = await wechat_util.getClient(code)
+    // client.clearQuota(appid, async function (err, data) {
+    //     console.log(err, '----------err')
+    //     console.log(data, '----------data')
+    // })
+    let times = await mem.get('dahao_script_clear_times_' + code)
+    console.log(times,'---------------------times')
 }
 a()
